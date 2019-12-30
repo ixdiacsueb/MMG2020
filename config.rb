e541f445f99135b95b2978a3abf3e8327e6b99ea
ian pollock <instructian@gmail.com>
2019-12-29 23:48:56 -0800

    uncompressed

--- config.rb
+++ config.rb
@@ -70,8 +70,8 @@ set :deptEmail, "multimedia@csueastbay.edu"
 
 
 configure :build do
-   activate :minify_css
-   activate :minify_javascript
-   activate :minify_html
+   #activate :minify_css
+   #activate :minify_javascript
+   #activate :minify_html
    set :build_dir, 'docs'
  end
